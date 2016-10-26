ceeb4ddcf7afba96bad8c1dc8e6db54d1e4c5f64
astraiophos <jacobbfull@gmail.com>
2016-10-26 12:56:26 -0600

    Removed dependencies list in order to install on server

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ app_class = 'aemdewater.app:ConstructionDewatering'
 app_package_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tethysapp', app_package)
 
 ### Python Dependencies ###
-dependencies = [numpy, scipy, matplotlib]
+dependencies = []
 
 setup(
     name=release_package,
