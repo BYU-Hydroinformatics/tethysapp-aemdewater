f5afdd734e7e9ef8b7b2a86d556b5c7d16604b18
astraiophos <jacobbfull@gmail.com>
2016-10-28 16:10:54 -0600

    File Paths 2

--- tethysapp/aemdewater/__init__.py
+++ tethysapp/aemdewater/__init__.py
@@ -14,6 +14,7 @@ except ImportError:
 __name__="matplotlib.pyplot"
 
 sys.path.append("/usr/local/lib/python2.7/dist-packages/matplotlib")
+sys.path.append("/usr/local/lib/python2.7/dist-packages/matplotlib/externals")
 sys.path.append("/usr/lib/pymodules/python2.7")
 sys.path.append("/usr/lib/python2.7/dist-packages")
 
@@ -21,4 +22,5 @@ from pyplot import *
 
 sys.path.remove("/usr/share/pyshared/matplotlib")
 sys.path.remove("/usr/lib/pymodules/python2.7")
+sys.path.remove("/usr/local/lib/python2.7/dist-packages/matplotlib/externals")
 sys.path.append("/usr/local/lib/python2.7/dist-packages/matplotlib")
